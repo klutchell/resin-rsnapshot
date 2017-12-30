c7d9b11fb1c2ee1da5464d1dec0cf29268bcb737
Kyle Harding <kylemharding@gmail.com>
2017-12-30 09:08:41 -0500

    add full command for remote hosts; check rsnapshot.conf syntax

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 id_rsa_key="/data/keys/id_rsa"
-ssh_config_file="/root/.ssh/config"
+ssh_conf_file="/root/.ssh/config"
+rsnapshot_conf_file="/etc/rsnapshot.conf"
 cron_file="/etc/cron.d/rsnapshot"
 
 make_secure_dir()
@@ -20,22 +21,24 @@ echo "configuring ssh..."
 make_secure_dir "$(dirname "${ssh_config_file}")"
 cp "/usr/src/app/ssh.conf" "${ssh_config_file}"
 
-echo "public key data (add to remote host authorized_keys):"
-cat "${id_rsa_key}.pub"
+echo "run this command on remote hosts:"
+echo "'echo \"$(cat "${id_rsa_key}.pub")\" >> ~/.ssh/authorized_keys'"
 
 echo "configuring rsnapshot..."
-cp "/usr/src/app/rsnapshot.conf" "/etc/rsnapshot.conf"
+cp "/usr/src/app/rsnapshot.conf" "${rsnapshot_conf_file}"
 
-[ -n "${BACKUP_POINT_0}" ] && echo "${BACKUP_POINT_0}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
-[ -n "${BACKUP_POINT_1}" ] && echo "${BACKUP_POINT_1}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
-[ -n "${BACKUP_POINT_2}" ] && echo "${BACKUP_POINT_2}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
-[ -n "${BACKUP_POINT_3}" ] && echo "${BACKUP_POINT_3}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
-[ -n "${BACKUP_POINT_4}" ] && echo "${BACKUP_POINT_4}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
-[ -n "${BACKUP_POINT_5}" ] && echo "${BACKUP_POINT_5}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
-[ -n "${BACKUP_POINT_6}" ] && echo "${BACKUP_POINT_6}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
-[ -n "${BACKUP_POINT_7}" ] && echo "${BACKUP_POINT_7}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
-[ -n "${BACKUP_POINT_8}" ] && echo "${BACKUP_POINT_8}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
-[ -n "${BACKUP_POINT_9}" ] && echo "${BACKUP_POINT_9}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
+[ -n "${BACKUP_POINT_0}" ] && echo "${BACKUP_POINT_0}" | sed 's/ \+ /\t/g' >> "${rsnapshot_conf_file}"
+[ -n "${BACKUP_POINT_1}" ] && echo "${BACKUP_POINT_1}" | sed 's/ \+ /\t/g' >> "${rsnapshot_conf_file}"
+[ -n "${BACKUP_POINT_2}" ] && echo "${BACKUP_POINT_2}" | sed 's/ \+ /\t/g' >> "${rsnapshot_conf_file}"
+[ -n "${BACKUP_POINT_3}" ] && echo "${BACKUP_POINT_3}" | sed 's/ \+ /\t/g' >> "${rsnapshot_conf_file}"
+[ -n "${BACKUP_POINT_4}" ] && echo "${BACKUP_POINT_4}" | sed 's/ \+ /\t/g' >> "${rsnapshot_conf_file}"
+[ -n "${BACKUP_POINT_5}" ] && echo "${BACKUP_POINT_5}" | sed 's/ \+ /\t/g' >> "${rsnapshot_conf_file}"
+[ -n "${BACKUP_POINT_6}" ] && echo "${BACKUP_POINT_6}" | sed 's/ \+ /\t/g' >> "${rsnapshot_conf_file}"
+[ -n "${BACKUP_POINT_7}" ] && echo "${BACKUP_POINT_7}" | sed 's/ \+ /\t/g' >> "${rsnapshot_conf_file}"
+[ -n "${BACKUP_POINT_8}" ] && echo "${BACKUP_POINT_8}" | sed 's/ \+ /\t/g' >> "${rsnapshot_conf_file}"
+[ -n "${BACKUP_POINT_9}" ] && echo "${BACKUP_POINT_9}" | sed 's/ \+ /\t/g' >> "${rsnapshot_conf_file}"
+
+/usr/bin/rsnapshot configtest
 
 echo "configuring cron..."
-cp "/usr/src/app/rsnapshot.cron" "/etc/cron.d/rsnapshot"
+cp "/usr/src/app/rsnapshot.cron" "${cron_file}"
