e52ecaa56ff77bc775ce71b530847a109b58f2b0
Kyle Harding <kylemharding@gmail.com>
2017-12-30 08:57:08 -0500

    move ssh config and rsnapshot cron to template files

--- start.sh
+++ start.sh
@@ -16,16 +16,16 @@ then
 	ssh-keygen -q -t "rsa" -N '' -f "${id_rsa_key}"
 fi
 
+echo "configuring ssh..."
 make_secure_dir "$(dirname "${ssh_config_file}")"
-echo "Host remotehost-rsnapshot" > "${ssh_config_file}"
-echo "Hostname *" >> "${ssh_config_file}"
-echo "IdentityFile ${id_rsa_key}" >> "${ssh_config_file}"
+cp "/usr/src/app/ssh.conf" "${ssh_config_file}"
 
 echo "public key data (add to remote host authorized_keys):"
 cat "${id_rsa_key}.pub"
 
-echo "writing rsnapshot config..."
+echo "configuring rsnapshot..."
 cp "/usr/src/app/rsnapshot.conf" "/etc/rsnapshot.conf"
+
 [ -n "${BACKUP_POINT_0}" ] && echo "${BACKUP_POINT_0}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
 [ -n "${BACKUP_POINT_1}" ] && echo "${BACKUP_POINT_1}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
 [ -n "${BACKUP_POINT_2}" ] && echo "${BACKUP_POINT_2}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
@@ -37,8 +37,5 @@ cp "/usr/src/app/rsnapshot.conf" "/etc/rsnapshot.conf"
 [ -n "${BACKUP_POINT_8}" ] && echo "${BACKUP_POINT_8}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
 [ -n "${BACKUP_POINT_9}" ] && echo "${BACKUP_POINT_9}" | sed 's/ \+/\t/g' >> "/etc/rsnapshot.conf"
 
-echo "scheduling cron jobs..."
-echo "0 */4 * * * root /usr/src/app/job.sh alpha" > "${cron_file}"
-echo "30 30 * * * root /usr/src/app/job.sh beta" >> "${cron_file}"
-echo "0 3 * * 1 root /usr/src/app/job.sh gamma" >> "${cron_file}"
-echo "30 2 1 * * root /usr/src/app/job.sh delta" >> "${cron_file}"
+echo "configuring cron..."
+cp "/usr/src/app/rsnapshot.cron" "/etc/cron.d/rsnapshot"
