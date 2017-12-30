f0492ec82291b3d1ddbe53d39984afa330757562
Kyle Harding <kylemharding@gmail.com>
2017-12-30 17:52:10 -0500

    shellcheck changes

--- job.sh
+++ job.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # /proc/1/fd/1 is systemd's stdout, and therefore appears in the resin logs.
-/usr/bin/rsnapshot -v -c /data/rsnapshot.conf ${1} >/proc/1/fd/1
+echo "running /usr/bin/rsnapshot -v -c /data/rsnapshot.conf ${1}..." &>/proc/1/fd/1
+/usr/bin/rsnapshot -v -c /data/rsnapshot.conf ${1} &>/proc/1/fd/1
