1d3e20c3d43c50c4cd701767262190141cd342de
Kyle Harding <kylemharding@gmail.com>
2017-12-30 14:01:57 -0500

    add comments

--- start.sh
+++ start.sh
@@ -15,6 +15,7 @@ spaces_to_tabs()
 	echo "${1}" | sed 's/ \+/\t/g'
 }
 
+# generate ssh key if one does not exist
 if [ ! -f "${id_rsa_key}" ]
 then
 	echo "generating ssh key..."
@@ -22,24 +23,27 @@ then
 	ssh-keygen -q -t "rsa" -N '' -f "${id_rsa_key}"
 fi
 
+# install ssh config file that specifies which key to use for all hosts
 echo "configuring ssh..."
 make_secure_dir "$(dirname "${ssh_conf_file}")"
 cp -a "/usr/src/app/ssh.conf" "${ssh_conf_file}"
 
+# print the command to add this public key to remote hosts
 echo "run this command on remote hosts:"
 echo "echo '$(cat "${id_rsa_key}.pub")' >> ~/.ssh/authorized_keys"
 
 rsnapshot_conf_required=false
 
-# if rsnapshot.conf does not exist or is not sane...
-/usr/bin/rsnapshot -c "${rsnapshot_conf_file}" configtest || rsnapshot_conf_required=true
+# reconfigure rsnapshot if config does not exist or is not sane
+/usr/bin/rsnapshot -c "${rsnapshot_conf_file}" configtest &>/dev/null || rsnapshot_conf_required=true
 
-# if any variable similar to RSNAPSHOT_CONF_ is set...
+# reconfigure rsnapshot if any variables starting with RSNAPSHOT_CONF_ are set
 for var in $(compgen -A variable | grep "^RSNAPSHOT_CONF_")
 do
 	[ -n "$(eval "echo \$${var}")" ] && rsnapshot_conf_required=true
 done
 
+# rsnapshot configuration is required for one of the above two reasons
 if [ "${rsnapshot_conf_required}" == true ]
 then
 	echo "configuring rsnapshot..."
@@ -50,16 +54,20 @@ then
 		if [ -n "$(eval "echo \$${var}")" ]
 		then
 			eval "echo +\$${var}"
+			# convert spaces to tabs and append each value to the end of rsnapshot.conf
 			spaces_to_tabs "$(eval "echo \$${var}")" >> "${rsnapshot_conf_file}"
 		fi
 	done
 fi
 
+# test rsnapshot configuration syntax
 /usr/bin/rsnapshot -c "${rsnapshot_conf_file}" configtest || exit 1
 
+# install cron.d file
 echo "configuring cron..."
 cp -a "/usr/src/app/rsnapshot.cron" "${cron_file}"
 
+# print cron schedules in human readable format
 IFS=$'\n'
 for schedule in $(cat /etc/cron.d/rsnapshot | grep 'root')
 do
