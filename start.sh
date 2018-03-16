c21de695b8d6d52854f72e6c74ed2c9d45defb9d
Kyle Harding <kylemharding@gmail.com>
2018-03-16 17:32:05 +0000

    flatten rsnapshot image and remove samba for now

---
+++ start.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# replace one or more spaces with a single tab
+spaces_to_tabs()	{ echo "${1}" | sed 's| \+|\t|g' ; }
+
+# create ssh dir if it doesn't exist
+if [ ! -d "${HOME}/.ssh" ]
+then
+	mkdir -p "${HOME}/.ssh"
+fi
+
+# generate ssh key to connect to other authenticated systems
+if [ ! -f "${HOME}/.ssh/id_rsa" ]
+then
+	ssh-keygen -q -t "rsa" -N '' -f "${HOME}/.ssh/id_rsa" -C "$(id -un)@$(hostname) $(date)"
+fi
+
+# generate ssh config file to prevent caching known hosts
+if [ ! -f "${HOME}/.ssh/config" ]
+then
+	cat <<EOF >> "${HOME}/.ssh/config"
+
+Host *
+	StrictHostKeyChecking no
+	UserKnownHostsFile /dev/null"
+
+EOF
+fi
+
+# set permissions on ssh dir
+chown -R root:root "${HOME}/.ssh"
+chmod -R 700 "${HOME}/.ssh"
+
+# mount /dev/sda1 if it exists
+if [ -e /dev/sda1 ]
+then
+	echo "mounting /dev/sda1 onto /snapshots ..."
+	mkdir /snapshots
+	mount /dev/sda1 /snapshots
+else
+	# rsnapshot configtest will fail since /snapshots does not exist
+	# and no_create_root is currently enabled
+	echo "no usb storage partitions found!"
+fi
+
+# append RSNAPSHOT_CONF_* environment variables to rsnapshot.conf
+echo "updating rsnapshot config ..."
+rsnapshot_config="/etc/rsnapshot.conf"
+for var in $(compgen -A variable | grep "^RSNAPSHOT_CONF_")
+do
+	if [ -n "$(eval "echo \$${var}")" ]
+	then
+		eval "echo +\$${var}"
+		spaces_to_tabs "$(eval "echo \$${var}")" >> "${rsnapshot_config}"
+	fi
+done
+
+# test rsnapshot config syntax
+echo "checking rsnapshot config ..."
+/usr/bin/rsnapshot -c "${rsnapshot_config}" configtest || exit 1
+
+# print cron schedule in human readable format
+echo "reading cron schedule ..."
+grep -v '^\s*#' "/etc/crontabs/root" | grep -v '^\s*$' | while IFS=$'\n' read -r line
+do
+	cmd="$(echo "${line}" | awk '{$1=$2=$3=$4=$5=""; print $0}' | sed -e 's/^[ \t]*//')"
+	exp="$(echo "${line}" | awk '{print $1"+"$2"+"$3"+"$4"+"$5}')"
+	sched="$(curl -s "https://cronexpressiondescriptor.azurewebsites.net/api/descriptor/?expression=${exp}&locale=en-US" | awk -F '"' '{print $4}')"
+	echo "+${cmd} @ ${sched}"
+done
+
+# start cron in foreground
+/usr/sbin/crond -f
