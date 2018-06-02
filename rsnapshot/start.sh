d632d7d9ce8da2761ae42ff76ccff372a8e7a86b
Kyle Harding <kylemharding@gmail.com>
2018-06-02 15:48:21 +0000

    restore usb mount

--- rsnapshot/start.sh
+++ rsnapshot/start.sh
@@ -7,18 +7,18 @@
 rsnapshot_config="/usr/src/app/rsnapshot.conf"
 crontab_schedule="/usr/src/app/crontab"
 
-# part_label="snapshots"
-# dev_path="$(blkid | grep "LABEL=\"${part_label}\"" | cut -d: -f1)"
+part_label="snapshots"
+dev_path="$(blkid | grep "LABEL=\"${part_label}\"" | cut -d: -f1)"
 
-# # mount device with LABEL="snapshot" if it exists
-# if [ -n "${dev_path}" ]
-# then
-# 	echo "mounting ${dev_path} onto ${SNAPSHOT_ROOT} ..."
-# 	mount "${dev_path}" "${SNAPSHOT_ROOT}"
-# else
-# 	echo "no devices with label '${part_label}' found!"
-# 	exit 1
-# fi
+# mount device with LABEL="snapshot" if it exists
+if [ -n "${dev_path}" ]
+then
+	echo "mounting ${dev_path} onto ${SNAPSHOT_ROOT} ..."
+	mount "${dev_path}" "${SNAPSHOT_ROOT}"
+else
+	echo "no devices with label '${part_label}' found!"
+	exit 1
+fi
 
 # append RSNAPSHOT_CONF_* environment variables to rsnapshot.conf
 echo "updating rsnapshot config ..."
