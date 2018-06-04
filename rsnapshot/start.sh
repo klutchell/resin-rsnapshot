b1e201c0fec03cb1df7837c524c5d258bbbb3946
Kyle Harding <kylemharding@gmail.com>
2018-06-04 12:39:27 +0000

    remove curl retry

--- rsnapshot/start.sh
+++ rsnapshot/start.sh
@@ -29,7 +29,7 @@ echo "checking rsnapshot config ..."
 echo "reading rsnapshot schedule ..."
 cat "${crontab_schedule}" | grep -v '^\s*#' | grep -v '^\s*$' \
 	| awk '{print "https://cronexpressiondescriptor.azurewebsites.net/api/descriptor/?expression="$1"+"$2"+"$3"+"$4"+"$5"&locale=en-US"}' \
-	| xargs curl -s --retry 3 | sed -r 's/\{"description":"([^"]+)"\}/\1\n/g'
+	| xargs curl -s | sed -r 's/\{"description":"([^"]+)"\}/\1\n/g'
 
 # start cron in foreground
 /usr/sbin/crond -f
